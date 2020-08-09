a1bd440572e8235cdd3dd2c7880f3d9df03f9dd4
aaronmontgomery <aaronmmontgomery@students.abtech.edu>
2020-08-09 11:00:12 -0400

    removed GetAccountPriceDeltasAsync moved the code processing accountPriceDeltas back into Worker

--- Stocks.Service/Worker.cs
+++ Stocks.Service/Worker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -32,11 +33,32 @@ namespace Stocks.Service
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            Dictionary<string, IEnumerable<Models.PriceDelta>> accountPriceDeltas;
+            Dictionary<Models.TdAmeritrade.Account.Account, IEnumerable<Models.PriceDelta>> accountPriceDeltas = new Dictionary<Models.TdAmeritrade.Account.Account, IEnumerable<Models.PriceDelta>>();
             while (!stoppingToken.IsCancellationRequested)
             {
-                accountPriceDeltas = await Modules.Merchant.GetAccountPriceDeltasAsync();
-                _logger.LogInformation("GetAccountPriceDeltasAsync completed: {time}", DateTime.UtcNow);
+                Entities.Authorization authorization = Modules.TdAmeritrade.Authorization.Update();
+                IEnumerable<Models.TdAmeritrade.Account.Account> accounts = Modules.TdAmeritrade.Account.Update(authorization);
+
+                // if account in accountPriceDeltas and not in accounts then remove from accountPriceDeltas
+                var accountPriceDeltasToRemove = accountPriceDeltas.Where(x => !accounts.Select(x => x.SecuritiesAccount.AccountId).Contains(x.Key.SecuritiesAccount.AccountId));
+                foreach (var accountPriceDeltaToRemove in accountPriceDeltasToRemove)
+                {
+                    accountPriceDeltas.Remove(accountPriceDeltaToRemove.Key);
+                }
+
+                // if account in accounts and not in accountPriceDeltas then add to accountPriceDeltas
+                var accountsToAdd = accounts.Where(x => !accountPriceDeltas.Select(x => x.Key.SecuritiesAccount.AccountId).Contains(x.SecuritiesAccount.AccountId));
+
+                // update existing accountPriceDeltas and add new accountPriceDeltas
+                foreach (Models.TdAmeritrade.Account.Account account in accounts)
+                {
+                    IEnumerable<Models.PriceDelta> priceDeltas = await Modules.Merchant.GetPriceDeltasAsync(account);
+                    _logger.LogInformation("GetPriceDeltasAsync completed: {time}", DateTime.UtcNow);
+                    priceDeltas = await Modules.Merchant.GetQuotesAsync(priceDeltas);
+                    _logger.LogInformation("GetQuotesAsync completed: {time}", DateTime.UtcNow);
+                    accountPriceDeltas.Add(account, priceDeltas);
+                }
+
                 await Task.Delay(60000, stoppingToken);
             }
         }
